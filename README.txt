083349847fec9690a592766d4f1b9580e8494960
Industry Dive <bomorin-id@MacBook-Pro.local>
2020-11-14 10:45:53 -0500

    Add readme, remove unused model junk

---
+++ README.txt
@@ -0,0 +1,8 @@
+Django Session Manager
+----------------------------
+
+This template repository comes with registrer, log in,
+and log out views to get a jump start on building a 
+Django project that requires user authentication.
+
+Extend or update the template files to customize.
--- session_manager/models.py
+++ session_manager/models.py
@@ -9,23 +9,8 @@ from session_manager.utils import twentyfourhoursfromnow
 
 
 class SessionManager(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=64)
-    expiration = models.DateTimeField(default=twentyfourhoursfromnow)
-
-    def generate_login_token(self):
-        token_base = '{}-{}-{}'.format(
-            self.user.email,
-            datetime.now(),
-            ''.join(random.choices(string.ascii_uppercase + string.digits, k = 60))
-        )
-        token_hash = hashlib.sha256(token_base.encode())
-        return token_hash.hexdigest()
-
-    def save(self, *args, **kwargs):
-        if not self.token:
-            self.token = self.generate_login_token()
-        super(LoginToken, self).save(*args, **kwargs)
+    class Meta:
+        abstract = True
 
     @classmethod
     def user_exists(cls, email):
