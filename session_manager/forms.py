556fa0300356b528a4a192589bfe603e04517fc6
Bo Morin <morinbe@gmail.com>
2020-12-05 18:38:50 -0500

    Add comments to form custom clean method

--- session_manager/forms.py
+++ session_manager/forms.py
@@ -22,6 +22,8 @@ class CreateUserForm(ModelForm):
         }
 
     def clean(self):
+        """ Enforces username and password requirements
+        """
         super(CreateUserForm, self).clean()
         data = self.cleaned_data
         errors = []
@@ -30,9 +32,9 @@ class CreateUserForm(ModelForm):
         has_number = False
         has_special = False
         clean_password = data['password']
-        pw_errors = []
+        password_errors = []
         if len(clean_password) < 8:
-            pw_errors.append('<li>Must be at least 8 characters.</li>')
+            password_errors.append('<li>Must be at least 8 characters.</li>')
         for char in clean_password:
             if char.isalpha():
                 has_alpha = True
@@ -41,33 +43,45 @@ class CreateUserForm(ModelForm):
             if char in special_chars:
                 has_special = True
         if not has_alpha:
-            pw_errors.append('<li>Must contain at least one letter.</li>')
+            password_errors.append('<li>Must contain at least one letter.</li>')
         if not has_number:
-            pw_errors.append('<li>Must contain at least one number.</li>')
+            password_errors.append('<li>Must contain at least one number.</li>')
         if not has_special:
-            pw_errors.append('<li>Must contain at least one special character ({}).</li>'.format(', '.join(special_chars)))
+            password_errors.append(
+                '<li>Must contain at least one special character ({}).</li>'.format(
+                    ', '.join(special_chars)
+                )
+            )
 
-        if pw_errors:
-            errors.append('Invalid password: <ul>{}</ul>'.format(''.join(pw_errors)))
+        if password_errors:
+            errors.append(
+                'Invalid password: <ul>{}</ul>'.format(''.join(password_errors))
+            )
 
         clean_username = self.cleaned_data['username']
-        un_errors = []
+        username_errors = []
         if len(clean_username) < 3:
-            un_errors.append('<li>Must be at least 3 characters.</li>')
+            username_errors.append('<li>Must be at least 3 characters.</li>')
 
-        un_invalid_char = False
+        username_invalid_char = False
         for char in clean_username:
-            un_invalid_char = not char.isalpha() and not char.isnumeric() and char != '_'
+            username_invalid_char = not char.isalpha() and not char.isnumeric() and char != '_'
 
-        if un_invalid_char:
-            un_errors.append('<li>Letters, numbers, and underscores only, please.</li>')
+        if username_invalid_char:
+            username_errors.append(
+                '<li>Letters, numbers, and underscores only, please.</li>'
+            )
 
         user_exists = SessionManager.get_user_by_username(username=clean_username)
         if user_exists:
-            un_errors.append('<li>A user with this username already exists.</li>')
+            username_errors.append(
+                '<li>A user with this username already exists.</li>'
+            )
 
-        if un_errors:
-            errors.append('Invalid username: <ul>{}</ul>'.format(''.join(un_errors)))
+        if username_errors:
+            errors.append(
+                'Invalid username: <ul>{}</ul>'.format(''.join(username_errors))
+            )
 
         if errors:
             raise ValidationError(mark_safe('<br />'.join(errors)))
